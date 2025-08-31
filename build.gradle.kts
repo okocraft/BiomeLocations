59ff6136b63fc13d462c397cedaca9d1d6ea4e3c
Siroshun09 <siro.shun8@gmail.com>
2025-08-31 23:27:38 +0900

    build: use gradle-plugins for commonize build logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,71 +1,41 @@
 plugins {
     `java-library`
-    id("com.gradleup.shadow") version "9.0.2"
+    id("dev.siroshun.gradle.plugins.jcommon") version "1.5.1"
+    id("dev.siroshun.gradle.plugins.bundler") version "1.5.1"
 }
 
 group = "net.okocraft.biomelocations"
 version = "1.3"
 
-val apiVersion = "1.20"
-val javaVersion = JavaVersion.VERSION_21
+val apiVersion = "1.21"
 
-java {
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
-}
+jcommon {
+    javaVersion = JavaVersion.VERSION_21
 
-repositories {
-    mavenCentral()
+    setupPaperRepository()
 
-    maven {
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
-}
+    commonDependencies {
+        compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 
-val configAPIVersion = "5.0.0-rc.2"
-
-dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    implementation("dev.siroshun.configapi:configapi-core:$configAPIVersion")
-    implementation("dev.siroshun.configapi:configapi-format-binary:$configAPIVersion")
-    implementation("dev.siroshun.configapi:configapi-format-yaml:$configAPIVersion") {
-        exclude("org.yaml", "snakeyaml")
-    }
-    implementation("dev.siroshun.configapi:configapi-serialization-record:$configAPIVersion")
-
-    implementation("dev.siroshun.mcmsgdef:mcmsgdef:1.0.0-rc.2")
-}
-
-tasks {
-    build {
-        dependsOn(shadowJar)
-    }
-
-    compileJava {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(javaVersion.ordinal + 1)
-    }
-
-    processResources {
-        filesMatching(listOf("plugin.yml")) {
-            expand(
-                    "apiVersion" to apiVersion,
-                    "projectVersion" to version,
-            )
+        val configAPIVersion = "5.0.0-rc.2"
+        implementation("dev.siroshun.configapi:configapi-core:$configAPIVersion")
+        implementation("dev.siroshun.configapi:configapi-format-binary:$configAPIVersion")
+        implementation("dev.siroshun.configapi:configapi-format-yaml:$configAPIVersion") {
+            exclude("org.yaml", "snakeyaml")
         }
-    }
+        implementation("dev.siroshun.configapi:configapi-serialization-record:$configAPIVersion")
 
-    jar {
-        archiveFileName = "BiomeLocations-$version-original.jar"
-        manifest {
-            attributes("paperweight-mappings-namespace" to "mojang")
-        }
+        implementation("dev.siroshun.mcmsgdef:mcmsgdef:1.0.0-rc.2")
     }
+}
 
-    shadowJar {
-        minimize()
-        relocate("com.github.siroshun09.configapi", "net.okocraft.biomelocations.libs.configapi")
-        relocate("dev.siroshun.mcmsgdef", "net.okocraft.biomelocations.libs.mcmsgdef")
-        archiveFileName = "BiomeLocations-$version.jar"
-    }
+bundler {
+    copyToRootBuildDirectory("BiomeLocations-${project.version}")
+    replacePluginVersionForBukkit(project.version, apiVersion)
+}
+
+tasks.shadowJar {
+    minimize()
+    relocate("com.github.siroshun09.configapi", "net.okocraft.biomelocations.libs.configapi")
+    relocate("dev.siroshun.mcmsgdef", "net.okocraft.biomelocations.libs.mcmsgdef")
 }
